fbd5d71d4e298b534dd2509a644504476c999215
kaipov24 <kaipovich24@gmail.com>
2025-04-07 18:02:02 +0600

    Reset update

--- reset.go
+++ reset.go
@@ -1,13 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func (cfg *apiConfig) handleReset(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("Resetting number of requests")))
+	if cfg.platform != "dev" {
+		w.WriteHeader(http.StatusForbidden)
+		w.Write([]byte("Reset is only allowed in dev environment."))
+		return
+	}
+
 	cfg.fileserverHits.Store(0)
+	cfg.db.Reset(r.Context())
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits reset to 0 and database reset to initial state."))
 }
