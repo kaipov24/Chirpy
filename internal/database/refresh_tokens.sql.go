e8f2d0c9c95d44c60c2fdfc514258101a1276f93
kaipov24 <kaipovich24@gmail.com>
2025-04-10 21:24:49 +0600

    Refresh token db vol 3

--- internal/database/refresh_tokens.sql.go
+++ internal/database/refresh_tokens.sql.go
@@ -7,7 +7,6 @@ package database
 
 import (
 	"context"
-	"database/sql"
 	"time"
 
 	"github.com/google/uuid"
@@ -19,15 +18,13 @@ INSERT INTO refresh_tokens (
     created_at,
     updated_at,
     user_id,
-    expires_at,
-    revoked_at)
+    expires_at)
 VALUES (
      $1,
      NOW(),
      NOW(),
      $2,
-     $3,
-     $4
+     $3
 )
 RETURNING token, created_at, updated_at, expires_at, revoked_at, user_id
 `
@@ -36,16 +33,52 @@ type CreateRefreshTokenParams struct {
 	Token     string
 	UserID    uuid.UUID
 	ExpiresAt time.Time
-	RevokedAt sql.NullTime
 }
 
 func (q *Queries) CreateRefreshToken(ctx context.Context, arg CreateRefreshTokenParams) (RefreshToken, error) {
-	row := q.db.QueryRowContext(ctx, createRefreshToken,
-		arg.Token,
-		arg.UserID,
-		arg.ExpiresAt,
-		arg.RevokedAt,
+	row := q.db.QueryRowContext(ctx, createRefreshToken, arg.Token, arg.UserID, arg.ExpiresAt)
+	var i RefreshToken
+	err := row.Scan(
+		&i.Token,
+		&i.CreatedAt,
+		&i.UpdatedAt,
+		&i.ExpiresAt,
+		&i.RevokedAt,
+		&i.UserID,
 	)
+	return i, err
+}
+
+const getUserFromRefreshToken = `-- name: GetUserFromRefreshToken :one
+SELECT users.id, users.created_at, users.updated_at, users.email, users.hashed_password FROM users
+JOIN refresh_tokens ON users.id = refresh_tokens.user_id
+WHERE refresh_tokens.token = $1
+AND revoked_at IS NULL
+AND expires_at > NOW()
+`
+
+func (q *Queries) GetUserFromRefreshToken(ctx context.Context, token string) (User, error) {
+	row := q.db.QueryRowContext(ctx, getUserFromRefreshToken, token)
+	var i User
+	err := row.Scan(
+		&i.ID,
+		&i.CreatedAt,
+		&i.UpdatedAt,
+		&i.Email,
+		&i.HashedPassword,
+	)
+	return i, err
+}
+
+const revokeRefreshToken = `-- name: RevokeRefreshToken :one
+UPDATE refresh_tokens SET revoked_at = NOW(),
+updated_at = NOW()
+WHERE token = $1
+RETURNING token, created_at, updated_at, expires_at, revoked_at, user_id
+`
+
+func (q *Queries) RevokeRefreshToken(ctx context.Context, token string) (RefreshToken, error) {
+	row := q.db.QueryRowContext(ctx, revokeRefreshToken, token)
 	var i RefreshToken
 	err := row.Scan(
 		&i.Token,
