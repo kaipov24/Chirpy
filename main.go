be0a870fa203759f29b4caaecea26c5acc2aeac4
kaipov24 <kaipovich24@gmail.com>
2025-04-11 13:58:46 +0600

    User update

--- main.go
+++ main.go
@@ -67,6 +67,7 @@ func main() {
 	mux.HandleFunc("POST /api/login", apiCfg.handlerLogin)
 	mux.HandleFunc("POST /api/refresh", apiCfg.handlerRefresh)
 	mux.HandleFunc("POST /api/revoke", apiCfg.handlerRevoke)
+	mux.HandleFunc("PUT /api/users", apiCfg.handlerUserUpdate)
 
 	// admin endpoints
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handleMetrics)
